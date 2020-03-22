d5b54fca0a5c9efe0436845cb07e06ea52823a3d
madcap15 <madcap15@tut.by>
2020-03-22 22:44:37 +0300

    Update dowloadrepo.sh

--- dowloadrepo.sh
+++ dowloadrepo.sh
@@ -4,7 +4,8 @@
 #url.txt - file with name of files list from site
 cat url.txt | cut -d' ' -f1 > url
 cat repodata.txt | cut -d' ' -f1 > repodata/url
-
+sleep 5
+echo "New files was created"
 link_files = "https:\/\/repo.zabbix.com\/zabbix\/"
 link_repodata = "https:\/\/repo.zabbix.com\/zabbix\/repo\/"
 
