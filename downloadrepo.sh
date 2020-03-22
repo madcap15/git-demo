50e18b9a34ea8c3f308ae9ad7426706e4a01758d
madcap15 <madcap15@tut.by>
2020-03-22 23:00:34 +0300

    First version

---
+++ downloadrepo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "First bash-script"
+
+#url.txt - file with name of files list from site
+cat url.txt | cut -d' ' -f1 > url
+cat repodata.txt | cut -d' ' -f1 > repodata/url
+
+sleep 5
+echo "New files was created"
+
+link_files = "https:\/\/repo.zabbix.com\/zabbix\/"
+link_repodata = "https:\/\/repo.zabbix.com\/zabbix\/repo\/"
+
+sed -i "zabbix/s/^/$link_files/" url
+sed -i "s/^/$link_repodata/" repodata/url
+echo "==============="
+wget -i url
+wget -i repodata/url
+echo "=============="
