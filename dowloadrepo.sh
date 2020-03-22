60278651fa97969d0f18665690bc4b104f657589
madcap15 <madcap15@tut.by>
2020-03-22 22:41:49 +0300

    First version

---
+++ dowloadrepo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+#url.txt - file with name of files list from site
+cat url.txt | cut -d' ' -f1 > url
+cat repodata.txt | cut -d' ' -f1 > repodata/url
+
+link_files = "https:\/\/repo.zabbix.com\/zabbix\/"
+link_repodata = "https:\/\/repo.zabbix.com\/zabbix\/repo\/"
+
+sed -i "zabbix/s/^/$link_files/" url
+sed -i "s/^/$link_repodata/" repodata/url
+echo "==============="
+wget -i url
+wget -i repodata/url
